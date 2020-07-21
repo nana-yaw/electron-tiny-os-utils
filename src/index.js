876ea18ad5eb081e3c655bbdc5e7a6701d7f1dad
nana-yaw <edward.osei.nyarko@gmail.com>
2020-07-21 18:12:20 +0000

    Basic logic for CPU Monitoring added

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const { app, BrowserWindow } = require('electron');
+const os = require('os-utils');
 const path = require('path');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
@@ -6,18 +7,73 @@ if (require('electron-squirrel-startup')) { // eslint-disable-line global-requir
   app.quit();
 }
 
+let checkCPU;
+let iconPath;
+
+if (process.platform === "linux") {
+
+  iconPath = __dirname + '/assets/images/icon.png'
+
+} else if(process.platform === "win32") {
+
+  iconPath = __dirname + '/assets/images/icon.ico'
+
+}else{
+
+  iconPath = __dirname + '/assets/images/icon.icns'
+
+}
+
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
+    width: 1000,
     height: 600,
+    icon: iconPath,
+    webPreferences: {
+      nodeIntegration: true
+    }
   });
 
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
+
+  mainWindow.webContents.on('did-finish-load', ()=>{
+    
+    checkCPU = setInterval(() => {
+        
+        os.cpuUsage((value) => {
+          // console.log(`CPU Usage: ${value*100} %`);
+          mainWindow.webContents.send('cpu',value*100)
+          // console.log(`Memory Usage: ${os.freememPercentage()*100} %`);
+          mainWindow.webContents.send('free-memory',os.freememPercentage()*100)
+          // console.log(`Total Memory: ${os.totalmem()/1024} GB`);
+          mainWindow.webContents.send('total-memory',os.totalmem()/1024)
+          
+        });
+        
+      }, 1000);
+
+  })
+
+  mainWindow.on('close', function(e){
+    var choice = require('electron').dialog.showMessageBox(this,
+        {
+          type: 'question',
+          buttons: ['Yes', 'No'],
+          title: 'Confirm',
+          message: 'Are you sure you want to quit?'
+       });
+       if(choice == 1){
+         e.preventDefault();
+       }else{
+        clearInterval(checkCPU)
+       }
+    });
+
 };
 
 // This method will be called when Electron has finished
